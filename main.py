775b281d594ebdb1faff84a49a2d351d61e10a88
SEM <sl4utinm@yandex.ru>
2022-10-26 14:12:42 +0500

    bubble sort 2

--- main.py
+++ main.py
@@ -3,14 +3,29 @@ n=int(input("введите кол-во цифр: "))
 a=[]
 for i in range(n):
     a.append(int(input()))
-i=0
-while i<n-1:
-    if(i>=0):
-        if a[i] > a[i + 1]:
-            a[i], a[i + 1] = a[i + 1], a[i]
-            i -= 1
+
+check=int(input("Выберите как отсортировать:\n1)По возрастанию\n2)По убыванию\n"))
+if(check==1):
+    i = 0
+    while i < n - 1:
+        if (i >= 0):
+            if a[i] > a[i + 1]:
+                a[i], a[i + 1] = a[i + 1], a[i]
+                i -= 1
+            else:
+                i += 1
         else:
-            i+=1
-    else:
-        i += 1
+            i += 1
+else:
+    i = 0
+    while i < n - 1:
+        if (i >= 0):
+            if a[i] < a[i + 1]:
+                a[i], a[i + 1] = a[i + 1], a[i]
+                i -= 1
+            else:
+                i += 1
+        else:
+            i += 1
+
 print(a)
